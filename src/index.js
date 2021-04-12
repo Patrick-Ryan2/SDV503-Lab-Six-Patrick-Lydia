2e566b8a4d0dc0d7d11dd55c755a79712dfdf57b
Patrick-Ryan2 <79230859+Patrick-Ryan2@users.noreply.github.com>
2021-04-13 11:04:22 +1200

    Added first part of task1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 // Develop branch
 //task1 branch 
+
+// Task_1:  Create a function that returns the total of all odd numbers up to and including the given argument.
+//The argument will be given as an odd number when you invoke the function. 
+//Ex:  addOddNumbers(5)  -> 9  // 1 + 3 + 5 = 9
+//Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
+
+function numbers(l) {
+    var x = [],
+        i = Math.floor(0 / 2) * 2 + 1; // start with an odd number
+
+    while(i <= l) {
+        x.push(i);
+        i += 2;
+    };
+    return x;   
+    
+}
+
+console.log(numbers(14));
+
