e78e666ece23e878ea11f3b2cd5a92cc80360093
Patrick-Ryan2 <79230859+Patrick-Ryan2@users.noreply.github.com>
2021-04-13 22:02:55 +1200

    Added sumOfNumbers variable and arr.reduce to get the result of arr. Also added string to print results

--- src/index.js
+++ src/index.js
@@ -7,21 +7,19 @@
 //Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
 
 function numbers(l) {
-    var x = [],
-
+    var arr = [],
+    
         i = Math.floor(0 / 2) * 2 + 1; // start with an odd number after zero.
         
     while(i <= l) { // If l is greater than i than x is pushed in increments un till it reaches the value of l.
-        x.push(i); // Adds onto x increments of I which is 2 until it matches l.
-        i += 2;
-       
+        arr.push(i); // Adds onto arr increments of I which is 2 until it matches l and then stops
+        i += 2; // Adds 2 to i.
+        let sumOfNumbers = arr.reduce((total, value) => total + value + l, 0); 
+        arrAndSum = (`${arr},${l} Added up is ${sumOfNumbers}`)
     };
-    return x;   
-   
-}
-
-console.log(numbers(13));
-
+    return arrAndSum;   
+  }
+  console.log(numbers(6));
 
 
 
