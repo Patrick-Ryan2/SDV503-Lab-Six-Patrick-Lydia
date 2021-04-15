f0a78a60e07435e2ab64e01ad070f87b2902776c
Patrick-Ryan2 <79230859+Patrick-Ryan2@users.noreply.github.com>
2021-04-15 14:44:58 +1200

    Clarified and added code comments

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@
 //Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
 
 function numbers(l) {
-    var arr = [],
+    var arr = [],// Creates new empty arrays
     
-        i = Math.floor(0 / 2) * 2 + 1; // start with an odd number after zero.
+        i = Math.floor(0 / 2) * 2 + 1; // start with an odd number after zero. 
+      
         
-    while(i <= l) { // If l is greater than i than x is pushed in increments un till it reaches the value of l.
-        arr.push(i); // Adds onto arr increments of I which is 2 until it matches l and then stops
-        i += 2; // Adds 2 to i.
-        let sumOfNumbers = arr.reduce((total, value) => total + value + l, 0); 
-        arrAndSum = (`${arr},${l} Added up is ${sumOfNumbers}`)
+    while(i <= l) { // If l is greater than i than while loop starts and x is pushed in increments until it reaches the value of l.
+        arr.push(i); // Adds(pushes) onto arr increments of I which is 2 until it matches l and then stops
+        i += 2; // Adds 2 to i.Because if 2 is added its still an odd number which is all the array needs to print.
+     
+        let sumOfNumbers = arr.reduce((total, value) => total + value, 0); // sumOfNumbers = arr reduced to add up all the odd numbers in arr. 
+        arrAndSum = (`The odd numbers of ${l} are ${arr} and there total is ${sumOfNumbers}`)// Added arrAndSum string which prints the origin input and then its odd numbers from arr and then its total from sumOfNumbers
     };
-    return arrAndSum;   
+
+    return arrAndSum;  // Returns arrAndSum variable
+    
   }
-  console.log(numbers(6));
+  console.log(numbers(13)); // Console logs the result of numbers function using inputted variable as basis for functions argument.
 
 
 
